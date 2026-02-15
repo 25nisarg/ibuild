aa82ada7e2ced36d71c0d6c16105207039c0e526
Mistry Nisarg <mistry.nisarg.24@gmail.com>
2026-02-15 19:24:55 +0530

    Completed Featured Product Card and Section

--- components/products/product-card.tsx
+++ components/products/product-card.tsx
@@ -1,15 +1,56 @@
-interface Product{
-    id:number;
-    name:string;
-    description:string;
-    tags:string[];
-    votes:number;
-    isFeatured:boolean;
+import Link from "next/link";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
+import { Badge } from "../ui/badge";
+import { StarIcon } from "lucide-react";
+import VotingButtons from "@/components/products/voting-button";
+
+interface Product {
+    id: number;
+    name: string;
+    description: string;
+    tags: string[];
+    votes: number;
+    isFeatured: boolean;
 }
-export default function ProductCard({product}:{product:Product}){
-return(
-    <div>
-        {product.name}
-    </div>
-)
+export default function ProductCard({ product }: { product: Product }) {
+     const hasVoted = false;
+    return (
+        <Link href={`/products/${product.id}`}>
+            <Card className="group card-hover hover:bg-primary-foreground/10 border-solid border-gray-400 min-h-[200px]">
+        <CardHeader className="flex-1">
+          <div className="flex items-start gap-4">
+            <div className="flex-1 min-w-0">
+              <div className="flex items-center gap-2">
+                <CardTitle className="text-lg group-hover:text-primary transition-colors">
+                  {product.name}
+                </CardTitle>
+                {product.votes > 100 && (
+                  <Badge className="gap-1 bg-primary text-primary-foreground">
+                    <StarIcon className="size-3 fill-current" />
+                    Featured
+                  </Badge>
+                )}
+              </div>
+              <CardDescription>{product.description}</CardDescription>
+            </div>
+            {/** Voting buttons */}
+            <VotingButtons
+              hasVoted={hasVoted}
+              voteCount={product.votes}
+              productId={product.id}
+            />
+          </div>
+        </CardHeader>
+        <CardFooter>
+          <div className="flex items-center gap-2">
+            {product.tags?.map((tag) => (
+              <Badge variant="secondary" key={tag}>
+                {tag}
+              </Badge>
+            ))}
+          </div>
+        </CardFooter>
+      </Card>
+        </Link>
+    )
 }
